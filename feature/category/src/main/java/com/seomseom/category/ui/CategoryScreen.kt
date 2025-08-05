2cdff7a74f8d2a7d72051b2d3b6d1e7a146403dd
DESKTOP-56ABDKU\USER-PC <aurora32s@naver.com>
2025-08-05 23:54:10 +0900

    [FEATURE] #14 - 밈 리스트 추가

--- feature/category/src/main/java/com/seomseom/category/ui/CategoryScreen.kt
+++ feature/category/src/main/java/com/seomseom/category/ui/CategoryScreen.kt
@@ -6,13 +6,21 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mimu_bird.designsystem.theme.Gray10
+import com.mimu_bird.designsystem.theme.Headline1
+import com.mimu_bird.designsystem.theme.White
+import com.mimu_bird.designsystem.typography.toTextStyle
+import com.mimu_bird.ui.component.MimSearchItem
 import com.mimu_bird.ui.model.CategoryUiModel
+import com.mimu_bird.ui.model.MimUiModel
+import com.mimu_bird.ui.model.TEST_BRIEF_MEME_UI
 import com.seomseom.category.component.CategoryTab
 
 @Preview
@@ -46,12 +54,38 @@ fun CategoryScreen(
             onSelectCategory = {}
         )
         LazyVerticalGrid(
+            modifier = Modifier
+                .weight(1f)
+                .background(Gray10),
             columns = GridCells.Fixed(2),
             contentPadding = PaddingValues(vertical = 10.dp, horizontal = 14.dp),
             horizontalArrangement = Arrangement.spacedBy(12.dp),
             verticalArrangement = Arrangement.spacedBy(12.dp)
         ) {
-
+            item (
+                span = { GridItemSpan(2) }
+            ){
+                Text(
+                    modifier = Modifier.padding(bottom = 10.dp),
+                    text = "선택된 카테고리 이름",
+                    style = Headline1.toTextStyle(),
+                    color = White
+                )
+            }
+            items(count = 20, key = { it }) {
+                MimSearchItem(
+                    meme = MimUiModel(
+                        id = it,
+                        imageUrl = "",
+                        year = "2025",
+                        title = "밈 $it",
+                        tags = emptyList(),
+                        usage = "",
+                        source = ""
+                    ),
+                    isKeyword = false
+                )
+            }
         }
     }
 }

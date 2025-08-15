3d5ba76bc6ffc93b4692b7aebec7f82a3e9ad5ec
DESKTOP-56ABDKU\USER-PC <aurora32s@naver.com>
2025-08-15 23:13:43 +0900

    [-> develop | Deploy] 1.1.0 업데이트

--- build-logic/convention/src/main/kotlin/const/Constants.kt
+++ build-logic/convention/src/main/kotlin/const/Constants.kt
@@ -7,8 +7,8 @@ object Constants {
     const val compileSdk = 35
     const val minSdk = 28
     const val targetSdk = 35
-    const val versionCode = 2
-    const val versionName = "1.0.0"
+    const val versionCode = 3
+    const val versionName = "1.1.0"
     val javaVersion = JavaVersion.VERSION_11
     val jvmVersion = "11"
 }

b6c64aa402044a27778d4c4ee27cbb9f41b50614
이용인 <yongin0517@gmail.com>
2025-08-22 21:18:28 +0900

    [Chore] 카카오 앱키 local.properties 값 적용

--- app/src/main/java/com/mimu_bird/meme/MemeApplication.kt
+++ app/src/main/java/com/mimu_bird/meme/MemeApplication.kt
@@ -3,11 +3,22 @@ package com.mimu_bird.meme
 import android.app.Application
 import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
+import java.io.File
+import java.util.Properties
 
 @HiltAndroidApp
 class MemeApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        KakaoSdk.init(this, "")
+        
+        // local.properties에서 카카오 앱 키 읽기
+        val localProperties = Properties()
+        val localPropertiesFile = File(filesDir.parentFile?.parentFile, "local.properties")
+        if (localPropertiesFile.exists()) {
+            localProperties.load(localPropertiesFile.inputStream())
+        }
+        val kakaoAppKey = localProperties.getProperty("kakao.app.key") ?: ""
+        
+        KakaoSdk.init(this, kakaoAppKey)
     }
 }

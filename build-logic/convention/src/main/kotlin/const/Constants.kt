f8cb13be3e76ec75f28f13f68762653b77e95290
이용인 <yongin0517@gmail.com>
2025-08-22 20:54:12 +0900

    [DEPLOY] 빌드 버전 수정
    - 1.6.1 (11) 로 수정

--- build-logic/convention/src/main/kotlin/const/Constants.kt
+++ build-logic/convention/src/main/kotlin/const/Constants.kt
@@ -7,8 +7,8 @@ object Constants {
     const val compileSdk = 35
     const val minSdk = 28
     const val targetSdk = 35
-    const val versionCode = 10
-    const val versionName = "1.6.0"
+    const val versionCode = 11
+    const val versionName = "1.6.1"
     val javaVersion = JavaVersion.VERSION_11
     val jvmVersion = "11"
 }

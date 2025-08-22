300db4fbdc3f217796d26793d51f0701f4354b77
이용인 <yongin0517@gmail.com>
2025-08-22 22:42:15 +0900

    [DEPLOY] 빌드 버전 수정
    - 1.6.1 (13) 로 수정
    - local.properties 파일 경로 잘못된 김에 걍 지워버리고 재업로드(?)

--- app/src/main/java/com/mimu_bird/meme/MemeApplication.kt
+++ app/src/main/java/com/mimu_bird/meme/MemeApplication.kt
@@ -3,22 +3,11 @@ package com.mimu_bird.meme
 import android.app.Application
 import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
-import java.io.File
-import java.util.Properties
 
 @HiltAndroidApp
 class MemeApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        
-        // local.properties에서 카카오 앱 키 읽기
-        val localProperties = Properties()
-        val localPropertiesFile = File(filesDir.parentFile?.parentFile, "local.properties")
-        if (localPropertiesFile.exists()) {
-            localProperties.load(localPropertiesFile.inputStream())
-        }
-        val kakaoAppKey = localProperties.getProperty("kakao.app.key") ?: ""
-        
-        KakaoSdk.init(this, kakaoAppKey)
+        KakaoSdk.init(this, "2809949a56a1ee4334ca8abf60e4a392")
     }
 }
